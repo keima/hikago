c3f668364c894a46412e232c843b5f6fe753eb7c
IMANANA, Kouta <keima@p-side.net>
2014-10-27 20:44:57 +0900

    gochannel 3

--- main.go
+++ main.go
@@ -2,31 +2,43 @@ package main
 
 import (
     "fmt"
-    "os"
 )
 
-  const goroutines = 10
+func main() {
 
-  func main() {
-    counter := make(chan int, 1)
+  a := make(chan int)
+  b := make(chan int)
+  c := make(chan int)
 
-    for i := 0; i < goroutines; i++ {
-      go func(counter chan int) {
-        value := <-counter
-        value++
-        fmt.Println("count", value)
-
-        if value == goroutines {
-          os.Exit(0)
-        }
-
-        counter <- value
-      }(counter)
-    }
-
-    counter <- 0
-
+  go func() {
     for {
+      fmt.Print(".")
+      a <- 0
+    }
+  }()
 
+  go func() {
+    for {
+      fmt.Print("*")
+      b <- 0
+    }
+  }()
+
+  go func() {
+    for {
+      fmt.Print("_")
+      c <- 0
+    }
+  }()
+
+  for i := 0; i < 10; i++ {
+    select {
+      case <-a:
+        fmt.Println("catch A")
+      case <-b:
+        fmt.Println("catch B")
+      case <-c:
+        fmt.Println("catch C")
     }
   }
+}
